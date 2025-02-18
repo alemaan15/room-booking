2cfbcaca99130cb96ef177a16977bcc6b611d69a
Adrian Hernandez <adrian.hernandez-monterroso@capgemini.com>
2025-02-18 15:09:25 +0100

    Connection with Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { RoomModule } from './room/room.module';
@@ -8,6 +9,7 @@ import { RoomModule } from './room/room.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/room-booking'),
     RoomModule,
   ],
   controllers: [],
--- src/room/entities/room.entity.ts
+++ src/room/entities/room.entity.ts
@@ -1,7 +1,28 @@
-export class Room {
+import { Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Room extends Document {
   id?: string;
-  name: string;
+  roomNumber: number;
+  roomFloor: number;
+  roomType: RoomType;
+  capacity: number;
+  price: number;
+  status: RoomStatus;
   description: string;
-  location: string;
   hotelIds: string[];
+}
+
+export enum RoomType {
+  STANDARD = 'standard',
+  DELUXE = 'deluxe',
+  SUITE = 'suite',
+}
+
+export enum RoomStatus {
+  AVAILABLE = 'available',
+  OCCUPIED = 'occupied',
+  RESERVED = 'reserved',
+  UNAVAILABLE = 'unavailable',
 }
