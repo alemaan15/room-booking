e094d2033de3280d9594ca5adfd659316fa7d6a3
Adrian Hernandez <adrianhernandezmonterroso@gmail.com>
2025-07-03 21:12:22 +0200

    feat: restructure room module and add user module with basic CRUD operations
    - Updated package.json to include new dependencies for mongoose and configuration management.
    - Refactored app.module.ts to integrate Mongoose and Config modules for MongoDB connection.
    - Removed old room DTOs, entities, controller, and service files; replaced with new structure under modules/room.
    - Created new user module with basic DTOs, entities, controller, and service for user management.
    - Added environment configuration for MongoDB connection.
    - Introduced prettier configuration for consistent code formatting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
@@ -11,25 +12,24 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  prettier,
   {
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
+      ecmaVersion: 2022, 
       sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
-  },
-  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
-  },
+  }
 );
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { RoomModule } from './room/room.module';
+import { RoomModule } from './modules/room/room.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configurationMongo } from './configuration/configuration-mongo';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [configurationMongo],
+      isGlobal: true,
+      envFilePath: `./env/${process.env.NODE_ENV}.env`,
+    }),
+    // MongooseModule.forRoot('mongodb://localhost:27017/roomBooking', {}),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('mongo.MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
     RoomModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
